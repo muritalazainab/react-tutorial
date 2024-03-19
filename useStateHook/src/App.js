1a6e2649169df62347e749ce398d47b44e1456e0
muritalazainab <muritala21@icloud.com>
2024-03-19 16:40:53 +0100

    first commit

--- useStateHook/src/App.js
+++ useStateHook/src/App.js
@@ -2,15 +2,12 @@
 import Header from "./Header";
 import Content from "./Content";
 import Footer from "./Footer";
-import Hero from "./Hero";
-import Welcome from "./Welcome";
+
 // import Nav from "./nav";
 
 function App() {
   return (
     <div className="App">
-      <Hero/>
-      <Welcome />
       <Header />
       <Content />
       <Footer />
--- useStateHook/src/Content.js
+++ useStateHook/src/Content.js
@@ -1,26 +1,61 @@
-import React from 'react'
+import {useState} from 'react'
 import './Content.css'
+import { FaTrash } from "react-icons/fa";
 
 const Content = () => {
-    const handleNameChange = () => {
-        const names = ["Fulani", "Oyee", "Eef"];
-        const int = Math.floor(Math.random() * 3);
-        return(names[int])
-    };
-    const handleClick = ()=> {
-      console.log("You clicked")
-    }
-    const handleClick2 = (name)=>{
-      console.log(`${name} was clicked`)
-    }
+  // const [name, setName] = useState('Bashirat')
+ const [items, setItem] = useState([
+  {
+    id: 1,
+    checked: false,
+    item: "A bag of garri"
+  },
+  {
+    id: 2,
+    checked: false,
+    item: "A bag of rice"
+  },
+  {
+    id: 3,
+    checked: false,
+    item: "A bag of semo"
+  },
+  {
+    id: 4,
+    checked: false,
+    item: "ponmo"
+  },
+])
+
+    // const handleNameChange = () => {
+    //     const names = ["Fulani", "Oyee", "Eef"];
+    //     const int = Math.floor(Math.random() * 3);
+    //    setName((names[int]))
+    // };
+   
+    // const handleClick2 = (name)=>{
+    //   console.log(`${name} was clicked`)
+    // }
     
   return (
     <main className='student'>
-      <h1>{handleNameChange()}</h1>
+      
+{/* 
+      <h1>{name}</h1>
+      <button onClick={handleNameChange}>Change Name</button>
       <button onClick={handleClick}>Click me</button>
-      <button onClick={ () =>{handleClick2('Hayzed')}}>Click me</button>
+      <button onClick={ () =>{handleClick2('Hayzed')}}>Click me</button> */}
+      <ul>
+        {items.map((item) => (
+          <li className='item' key={item.id}>
+            <input type="checkbox" checked={item.checked} />
+            <label>{item.item}</label>
+            <FaTrash />
+          </li>
+        ))}
+        </ul>
     </main>
-  )
-}
+  );
+};
 
 export default Content
