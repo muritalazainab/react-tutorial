b498197a3839f71339b7985506f387a92ef05a7b
muritalazainab <muritala21@icloud.com>
2024-04-03 13:02:41 +0100

    react-router

---
+++ react-router/src/NewPost.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewPost = () => {
+  return (
+    <main>
+      <h1>Newpost</h1>
+    </main>
+  )
+}
+
+export default NewPost
---
+++ react-router/src/PostPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PostPage = () => {
+  return (
+    <main>
+      <h1>postpages</h1>
+    </main>
+  )
+}
+
+export default PostPage
+
+
